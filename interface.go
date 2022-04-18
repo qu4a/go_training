0e526e81753bd50669c2920d82cbf0eb655dc1ec
Qu4a <4dorud@gmail.com>
2022-04-18 23:23:33 +0300

    interface

--- interface.go
+++ interface.go
@@ -1,15 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"mypkg"
+	"gadget"
 )
 
-func main() {
-	var value mypkg.MyInterface //объявление переменной и использованием типа Interface
-	value = mypkg.MyType(5.7)
-	value.MethodWithoutParameters()
-	value.MethodWithParameter(127.4)
-	fmt.Println(value.MethodWithReturnValue())
+type Play interface { //оперделяет тип интерфейся
+	Play(string) //должен содрежать метод Play c параметром стринг
+	Stop()
+}
 
+func playList(device Play, songs []string) { //допустимо любое значение, поддерживающее Plyaer, не только TapePlayer
+	for _, song := range songs {
+		device.Play(song)
+	}
+	device.Stop()
+}
+
+func main() {
+	mixplay := []string{"Jessie's Girl", "Whip It", "9 to 5"}
+	var player Play = gadget.TapePlayer{} //объявляем переменную для хранения любого значения, поддреживающего Player
+	playList(player, mixplay)
+	player = gadget.TapeRecorder{}
+	playList(player, mixplay)
 }
