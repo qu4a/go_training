96435b6dc3189b1e5f687d022a8fa4327792b397
Qu4a <4dorud@gmail.com>
2022-04-24 22:49:04 +0300

    test panic and recurses

---
+++ recurses.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+)
+
+func scanDirectory(path string) error {
+	fmt.Println(path)
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range files {
+		filePath := filepath.Join(path, file.Name())
+		if file.IsDir() {
+			err := scanDirectory(filePath)
+			if err != nil {
+				return err
+			}
+		} else {
+			fmt.Println(filePath)
+		}
+	}
+	return nil
+}
