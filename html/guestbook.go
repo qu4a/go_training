1b9728583aa94ff35e784e384e08a7abd57c1298
Qu4a <4dorud@gmail.com>
2022-05-10 22:04:15 +0300

    test

--- html/guestbook.go
+++ html/guestbook.go
@@ -4,12 +4,19 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 func check(err error) { //код сообщений об ошибках перемещается в эту функцию
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+func executeTemplate(text string, data interface{}) { //data значения передаются методу Execute шаблона
+	tmpl, err := template.New("test").Parse(text)
+	check(err)
+	err = tmpl.Execute(os.Stdout, data)
+	check(err)
 }
 
 func viewHandler(writer http.ResponseWriter, request *http.Request) {
@@ -22,9 +29,11 @@ func viewHandler(writer http.ResponseWriter, request *http.Request) {
 	check(err)
 	err = html.Execute(writer, nil) //содердимое шаблона записывается в ResponseWriter
 	check(err)
-}
+
 func main() {
 	http.HandleFunc("/guesbook", viewHandler)
 	err := http.ListenAndServe("localhost:8080", nil)
 	log.Fatal(err) //ошибка никогда не равно nil, по этому не вызываем check
+	executeTemplate("Dot is: {{.}}!\n", "ABC")
+	executeTemplate("Dot is: {{.}}!\n", 123.5)
 }
